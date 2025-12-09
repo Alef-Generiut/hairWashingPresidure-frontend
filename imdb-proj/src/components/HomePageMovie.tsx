813550150016a989786ce4d48d43f181f9a3ce88
gioeleMina <gioele.m.m@gmail.com>
2025-12-09 17:49:58 +0200

    SideImage.tsx:(pregress on the design)

--- imdb-proj/src/components/HomePageMovie.tsx
+++ imdb-proj/src/components/HomePageMovie.tsx
@@ -1,27 +1,23 @@
 import imageTemp from "../assets/Untitled.jpg";
-
+import "./HomePageMovie.css";
 import { Box, Text, Image } from "@mantine/core";
+import SideImage from "./SideImage";
+
 const HomePageMovie = () => {
   return (
-    <Box className="relative w-[70vw] h-96 mx-auto flex items-end justify-center rounded-md -z-10 mt-[13vh]">
-      {/* Large yellow glow behind the component */}
-      <Box className="absolute -z-10 w-[85vw] h-[85vh] rounded-md bg-yellow-400/60 blur-[100px] -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
-
-      {/* IMAGE WRAPPER — ONLY THIS HAS overflow-hidden */}
-      <Box className="absolute inset-0 w-full h-full rounded-md overflow-hidden z-10">
+    <Box className="headlineMovie">
+      <Box className="lowTaperFade" />
+      <Box className="imageHolder">
         <Image
           src={imageTemp}
           alt="Background"
-          className="w-full h-full object-cover"
+          className="frontImage"
           fit="cover"
         />
       </Box>
 
-      {/* Bottom yellow fade */}
-      <Box className="absolute bottom-0 left-0 w-full h-1/3 z-20 bg-linear-to-t from-[rgba(250,204,21,0.80)] to-transparent rounded-md" />
-
-      {/* Content */}
-      <Box className="relative z-30 p-6 text-white">
+      <Box className="textHolder">
+        <SideImage />
         <Text size="xl">Hello World</Text>
         <Text>Some description here</Text>
       </Box>
