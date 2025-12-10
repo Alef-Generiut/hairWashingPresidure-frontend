5b4fb7611b217e7fcf2a2c078c22001441902a5c
gioeleMina <gioele.m.m@gmail.com>
2025-12-10 13:27:12 +0200

    feat(FeatureMovie.tsx:component is complete design wise, no functionality)

--- imdb-proj/src/components/featureChoice/FeatureChoice.tsx
+++ imdb-proj/src/components/featureChoice/FeatureChoice.tsx
@@ -1,29 +1,24 @@
 import imageTemp from "../../assets/eternal-sunshine-of-the-spotless-mind.jpg";
 import image from "../../assets/enternal-logo.jpg";
 import { Box, Image, Text } from "@mantine/core";
-import "./FeaturedChoice.css";
+import "./FeatureChoice.css";
 
-const FeaturedChoice = () => {
+const FeatureChoice = () => {
   return (
     <Box className="featuredCard">
       <Box className="featuredBg">
-        <Image
-          src={imageTemp} 
-          className="featuredBgImg"
-          fit="cover"
-        />
+        <Image src={imageTemp} className="featuredBgImg" fit="cover" />
       </Box>
 
       <Box className="featuredContent">
         <Box className="featuredPoster">
           <Image src={image} className="posterImg" radius="md" fit="cover" />
         </Box>
-
-        <Text size="xl" className="featuredTitle">
-          'Inside Out' 2 Make us Feel Every Emotion
+        <Text size="sm" className="featuredTitle">
+          'Eternal Sunshine Of The Spotless Mind'
         </Text>
       </Box>
     </Box>
   );
 };
-export default FeaturedChoice;
+export default FeatureChoice;
--- imdb-proj/src/components/featureMovie/FeatureMovie.tsx
+++ imdb-proj/src/components/featureMovie/FeatureMovie.tsx
@@ -1,24 +1,45 @@
 import imageTemp from "../../assets/eternal-sunshine-of-the-spotless-mind.jpg";
 import "./FeatureMovie.css";
-import { Box, Text, Image } from "@mantine/core";
+import { Box, Image, Text } from "@mantine/core";
 import image from "../../assets/enternal-logo.jpg";
 import FeatureMovieDesc from "../featureMovieDesc/FeatureMovieDesc.tsx";
+import FeatureChoice from "../featureChoice/FeatureChoice.tsx";
 const HomePageMovie = () => {
   return (
     <Box className="headlineMovie">
       <Box className="lowTaperFade" />
 
-      <Box className="imageHolder">
-        <Image
-          src={imageTemp}
-          alt="Background"
-          className="frontImage"
-          fit="cover"
-        />
-      </Box>
+      <Box className="mainRow">
+        <Box className="featureMovieGroup">
+          <Box className="imageHolder">
+            <Image
+              src={imageTemp}
+              alt="Background"
+              className="frontImage"
+              fit="cover"
+            />
+          </Box>
 
-      <FeatureMovieDesc image={image} text={'Eternal Sunshine Of The Spotless Mind'} />
-      
+          <Box className="contentRow">
+            <FeatureMovieDesc
+              image={image}
+              text="Eternal Sunshine Of The Spotless Mind"
+            />
+          </Box>
+        </Box>
+        <Box className="features space-y-[4vh]">
+          <Box>
+            <Text className="flex justify-start" size="xs">
+              <span className="text-yellow-400 mx-0.5">●</span> Featured Choices
+            </Text>
+          </Box>
+          <Box>
+            <FeatureChoice />
+            <FeatureChoice />
+            <FeatureChoice />
+          </Box>
+        </Box>
+      </Box>
     </Box>
   );
 };
